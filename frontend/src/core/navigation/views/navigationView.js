1071b361689004a29750e664f4948af4b90cb3a8
Tom Taylor <hello@tomtaylor.name>
2017-03-10 18:15:29 +0000

    Refactor navigationView

--- frontend/src/core/navigation/views/navigationView.js
+++ frontend/src/core/navigation/views/navigationView.js
@@ -1,23 +1,20 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require){
-
   var OriginView = require('coreJS/app/views/originView');
   var Origin = require('coreJS/app/origin');
 
   var NavigationView = OriginView.extend({
-
     tagName: 'nav',
-
     className: 'navigation',
 
     initialize: function() {
-      this.listenTo(Origin, 'login:changed', this.loginChanged);
+      this.listenTo(Origin, 'user:updated', this.onLoginChanged);
       this.render();
     },
 
     events: {
-      'click a.navigation-item':'onNavigationItemClicked',
-      'click button.revert-loginas':'onRevertLoginas'
+      'click a.navigation-item': 'onNavigationItemClicked',
+      'click button.revert-loginas': 'onRevertLoginas'
     },
 
     render: function() {
@@ -27,37 +24,36 @@ define(function(require){
       return this;
     },
 
-    loginChanged: function() {
+    onLoginChanged: function() {
       this.render();
     },
 
     onRevertLoginas: function() {
-      var sessionModel = Origin.sessionModel;
-      // back up of the seesion
-      var _revertLogin = sessionModel.get('_revertLogin');
-      $.ajax({
-        method: 'post',
-        url: '/api/loginas',
-        data: {email: _revertLogin.email},
-        success: function (jqXHR, textStatus, errorThrown) {
-          sessionModel.set('_canRevert', false);
-          sessionModel.set('id', jqXHR.id);
-          sessionModel.set('tenantId', jqXHR.tenantId);
-          sessionModel.set('email', jqXHR.email);
-          sessionModel.set('isAuthenticated', jqXHR.success);
-          sessionModel.set('permissions', jqXHR.permissions);
-          delete sessionModel._revertLogin;
-          Origin.trigger('login:changed');
-          Origin.trigger('globalMenu:refresh');
-          Origin.router.navigate('#/dashboard', {trigger: true});
-        },
-        failure: function (err) {
-          Origin.Notify.alert({
-            type: 'error',
-            text: window.polyglot.t('app.errorlogginginas')
-          });
-        }
+      var data = { email: Origin.sessionModel.get('_revertLogin').email };
+      $.post( '/api/loginas', data, this.onRevertLoginasSuccess)
+        .fail(this.onRevertLoginasError);
       });
+    },
+
+    onRevertLoginasError: function() {
+      Origin.Notify.alert({ type: 'error', text: window.polyglot.t('app.errorlogginginas') });
+    },
+
+    onRevertLoginasSuccess: function(jqXHR, textStatus, errorThrown) {
+      var sessionModel = Origin.sessionModel;
+
+      sessionModel.set('_canRevert', false);
+      sessionModel.set('id', jqXHR.id);
+      sessionModel.set('tenantId', jqXHR.tenantId);
+      sessionModel.set('email', jqXHR.email);
+      sessionModel.set('isAuthenticated', jqXHR.success);
+      sessionModel.set('permissions', jqXHR.permissions);
+
+      delete sessionModel._revertLogin;
+
+      Origin.trigger('login:changed');
+      Origin.trigger('globalMenu:refresh');
+      Origin.router.navigate('#/dashboard', { trigger: true });
     },
 
     onNavigationItemClicked: function(event) {
@@ -65,11 +61,9 @@ define(function(require){
       event.stopPropagation();
       Origin.trigger('navigation:' + $(event.currentTarget).attr('data-event'));
     }
-
   }, {
     template: 'navigation'
   });
 
   return NavigationView;
-
 });
