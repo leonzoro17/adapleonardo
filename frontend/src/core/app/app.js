bc9ba51a663cef5d621353818820f4199ca4ed34
Tom Taylor <hello@tomtaylor.name>
2017-03-10 17:59:45 +0000

    Refactor app

--- frontend/src/core/app/app.js
+++ frontend/src/core/app/app.js
@@ -1,105 +1,105 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 require([
-    'templates',
-    'polyglot',
-    'sweetalert',
-    'coreJS/app/origin',
-    'coreJS/app/router',
-    'coreJS/app/permissions',
-    'coreJS/user/user',
-    'coreJS/project/project',
-    'coreJS/dashboard/dashboard',
-    'coreJS/editor/editor',
-    'coreJS/assetManagement/assetManagement',
-    'coreJS/pluginManagement/pluginManagement',
-    'coreJS/user/models/sessionModel',
-    'coreJS/navigation/views/navigationView',
-    'coreJS/globalMenu/globalMenu',
-    'coreJS/sidebar/sidebar',
-    'coreJS/app/helpers',
-    'coreJS/app/contextMenu',
-    'coreJS/location/location',
-    'plugins/plugins',
-    'coreJS/notify/notify',
-    'coreJS/editingOverlay/editingOverlay',
-    'coreJS/options/options',
-    'coreJS/scaffold/scaffold',
-    'coreJS/modal/modal',
-    'coreJS/filters/filters',
-    'coreJS/actions/actions',
-    'jquery-ui',
-    'jquery-form',
-    'inview',
-    'imageReady',
-    'mediaelement',
-    'velocity',
-    'scrollTo',
-    'ace/ace'
+  'templates',
+  'polyglot',
+  'sweetalert',
+  'coreJS/app/origin',
+  'coreJS/app/router',
+  'coreJS/app/permissions',
+  'coreJS/user/user',
+  'coreJS/project/project',
+  'coreJS/dashboard/dashboard',
+  'coreJS/editor/editor',
+  'coreJS/assetManagement/assetManagement',
+  'coreJS/pluginManagement/pluginManagement',
+  'coreJS/user/models/sessionModel',
+  'coreJS/navigation/views/navigationView',
+  'coreJS/globalMenu/globalMenu',
+  'coreJS/sidebar/sidebar',
+  'coreJS/app/helpers',
+  'coreJS/app/contextMenu',
+  'coreJS/location/location',
+  'plugins/plugins',
+  'coreJS/notify/notify',
+  'coreJS/editingOverlay/editingOverlay',
+  'coreJS/options/options',
+  'coreJS/scaffold/scaffold',
+  'coreJS/modal/modal',
+  'coreJS/filters/filters',
+  'coreJS/actions/actions',
+  'jquery-ui',
+  'jquery-form',
+  'inview',
+  'imageReady',
+  'mediaelement',
+  'velocity',
+  'scrollTo',
+  'ace/ace'
 ], function (
-    Templates,
-    Polyglot,
-    SweetAlert,
-    Origin,
-    Router,
-    Permissions,
-    User,
-    Project,
-    Dashboard,
-    Editor,
-    AssetManagement,
-    PluginManagement,
-    SessionModel,
-    NavigationView,
-    GlobalMenu,
-    Sidebar,
-    Helpers,
-    ContextMenu,
-    Location,
-    Notify,
-    EditingOverlay,
-    Options,
-    Scaffold,
-    Modal,
-    JQueryUI,
-    JQueryForm,
-    Inview,
-    ImageReady,
-    MediaElement
+  Templates,
+  Polyglot,
+  SweetAlert,
+  Origin,
+  Router,
+  Permissions,
+  User,
+  Project,
+  Dashboard,
+  Editor,
+  AssetManagement,
+  PluginManagement,
+  SessionModel,
+  NavigationView,
+  GlobalMenu,
+  Sidebar,
+  Helpers,
+  ContextMenu,
+  Location,
+  Notify,
+  EditingOverlay,
+  Options,
+  Scaffold,
+  Modal,
+  JQueryUI,
+  JQueryForm,
+  Inview,
+  ImageReady,
+  MediaElement
 ) {
-
-  // Read in the configuration values/constants
-  $.getJSON('config/config.json', function(configData) {
-    Origin.constants = configData;
-
-    var locale = localStorage.getItem('lang') || 'en';
-
-    // Get the language file
-    $.getJSON('lang/' + locale, function(data) {
-      // Instantiate Polyglot with phrases
-      window.polyglot = new Polyglot({phrases: data});
-
-      Origin.sessionModel = new SessionModel();
-
-      Origin.router = new Router();
-
-      Origin.sessionModel.fetch({
-        success: function(data) {
-          // This callback is called from the schemasModel.js in scaffold as the schemas
-          // need to load before the app loads
-          Origin.trigger('app:userCreated', function() {
-
-              $('#app').before(new NavigationView({model: Origin.sessionModel}).$el);
-              Origin.trigger('app:dataReady');
-              // Defer here is good - give anything tapping in app:dataReady event
-              // time to do their thang!
-              _.defer(function() {
-                  Origin.initialize();
-              });
-          });
-          
-        }
+  loadServerConfig(function() {
+    loadLanguageData(function() {
+      loadSessionData(function() {
+        Origin.router = new Router();
+        // FIXME the following is called from schemasModel as schemas need to load before the app
+        Origin.trigger('app:userCreated', function() {
+          $('#app').before(new NavigationView({ model: Origin.sessionModel }).$el);
+          Origin.trigger('app:dataReady');
+          // defer to give anything tapping app:dataReady time to execute
+          _.defer(Origin.initialize);
+        });
       });
     });
   });
-  
 });
+
+function loadServerConfig(callback) {
+  // Read in the configuration values/constants
+  $.getJSON('config/config.json', function(configData) {
+    // TODO name doesn't seem transparent enough
+    Origin.constants = configData;
+    callback.call(this);
+  });
+}
+
+// initialises the language and loads polyglot
+function loadLanguageData(callback) {
+  var locale = localStorage.getItem('lang') || 'en';
+  $.getJSON('lang/' + locale, function(data) {
+  window.polyglot = new Polyglot({ phrases: data });
+}
+
+function loadSessionData(callback) {
+  Origin.sessionModel = new SessionModel();
+  Origin.sessionModel.fetch();
+  Origin.on('sessionModel:initialised', callback);
+}
